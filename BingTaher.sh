da9b4653fa2e30b6a0b1759de2648e1b98d7c5ff
taherahmadi <14taher@gmail.com>
2016-04-22 19:26:09 +0430

    my script added

---
+++ BingTaher.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+PICTURE_DIR="$HOME/Pictures/bing-wallpapers/"
+
+mkdir -p $PICTURE_DIR
+
+wget -q --tries=10 --timeout=20 --spider http://google.com
+if [[ $? -eq 0 ]]; then
+        echo "Online"
+
+
+mkdir -p $PICTURE_DIR
+
+urls=( $(curl -s http://www.bing.com | \
+    grep -Eo "url:'.*?'" | \
+    sed -e "s/url:'\([^']*\)'.*/http:\/\/bing.com\1/" | \
+    sed -e "s/\\\//g") )
+
+for p in ${urls[@]}; do
+    filename=$(echo $p|sed -e "s/.*\/\(.*\)/\1/")
+    if [ ! -f $PICTURE_DIR/$filename ]; then
+        echo "Downloading: $filename ..."
+        curl -Lo "$PICTURE_DIR/$filename" $p
+    else
+        echo "Skipping: $filename ..."
+    fi
+done
+	
+	gsettings set org.gnome.desktop.background picture-uri file:///home/taher/Pictures/bing-wallpapers/$filename
+else
+        echo "Offline"
+fi
+
+
